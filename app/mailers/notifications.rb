bfc09c26f35c37f75c515ec040d6f742778404e9
Alice <aliceren@seas.upenn.edu>
2015-03-02 13:06:57 -0500

    configure mailer

---
+++ app/mailers/notifications.rb
@@ -0,0 +1,15 @@
+class Notifications < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifications.new_user.subject
+  #
+  def new_user(user)
+    @greeting = "Hi!"
+    @name = user.name
+    @email = user.email
+
+    mail to: @email
+  end
+end
