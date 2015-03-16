e6e47af2ff5f031ddddec70caa0c0a116598054f
Alice <aliceren@seas.upenn.edu>
2015-03-16 12:43:20 -0400

    reorder sendgrid config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,11 +7,11 @@ Rails.application.configure do
   # Configuration for mailer (sendgrid)
   config.action_mailer.default_url_options = {host: 'evening-fjord-8412.herokuapp.com'}
   config.action_mailer.smtp_settings = {
-    :address    => 'smtp.sendgrid.net',
-    :port       => 587,
     :user_name  => ENV['SENDGRID_USERNAME'],
     :password   => ENV['SENDGRID_PASSWORD'],
     :domain     => ENV['SENDGRID_DOMAIN'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
